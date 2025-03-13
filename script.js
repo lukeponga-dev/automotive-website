8503c34b6f43af3cd15a6fc758b49e9c08985030
Luke Ponga <51413507+lukeponga-dev@users.noreply.github.com>
2025-03-13 23:37:51 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
 
-    menuToggle.addEventListener("click", () => {
-        navLinks.classList.toggle("nav-active");
-    });
-});
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navLinks = document.querySelector(".nav-links");
